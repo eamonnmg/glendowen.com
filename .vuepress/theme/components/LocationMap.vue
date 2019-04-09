1a3655b053be585ab103eef5e38a04954d4cf998
eamonnmcgonigle <mcgonigle.eamonn@gmail.com>
2019-04-09 22:51:40 +0100

    import google-maps lib in mounted

--- .vuepress/theme/components/LocationMap.vue
+++ .vuepress/theme/components/LocationMap.vue
@@ -4,27 +4,28 @@
 </template>
 
 <script>
-    import GoogleMapsLoader from 'google-maps';
-
     const GLENDOWEN_COORDINATES = {lat: 55.2342023, lng: -7.4000257};
 
     export default {
         name: "location-map.vue",
         mounted(){
             const vm = this;
-            GoogleMapsLoader.KEY = 'AIzaSyDEMcKJO6HVsPg153Mln0kl9JUNYZgiA5g';
-            GoogleMapsLoader.VERSION = '3.34';
+            import('google-maps').then((GoogleMapsLoader)=>{
+                GoogleMapsLoader.KEY = 'AIzaSyDEMcKJO6HVsPg153Mln0kl9JUNYZgiA5g';
+                GoogleMapsLoader.VERSION = '3.34';
 
-            GoogleMapsLoader.load((google) => {
+                GoogleMapsLoader.load((google) => {
 
-                vm.googleMap =  new google.maps.Map(vm.$el, {
-                    zoom: 9,
-                    center: GLENDOWEN_COORDINATES,
+                    vm.googleMap =  new google.maps.Map(vm.$el, {
+                        zoom: 9,
+                        center: GLENDOWEN_COORDINATES,
+                    });
+
+
+                    vm.glendowenMarker = new google.maps.Marker({position: GLENDOWEN_COORDINATES, map: vm.googleMap});
                 });
-
-
-                vm.glendowenMarker = new google.maps.Marker({position: GLENDOWEN_COORDINATES, map: vm.googleMap});
             });
+
 
 
         }
