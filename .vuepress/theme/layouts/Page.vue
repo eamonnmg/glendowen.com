b972f4b3a3e08cc4b737e563c1a64a0245145018
eamonnmcgonigle <mcgonigle.eamonn@gmail.com>
2019-04-16 22:26:30 +0100

    fix p styles

--- .vuepress/theme/layouts/Page.vue
+++ .vuepress/theme/layouts/Page.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mx-auto max-w-xl">
         <main class="container border p-6 sm:m-6 ">
-            <article class="text-base font-sans leading-normal tracking-normal">
+            <article>
                 <header class="mb-6">
                     <h1>{{$page.frontmatter.title}}</h1>
                     <span>April 3, 2019 By eamonn</span>
