7ce131eb62dcb1b2b2c58af76318d9198b9abd71
eamonnmcgonigle <mcgonigle.eamonn@gmail.com>
2019-04-18 20:32:32 +0100

    get started on image gallary

---
+++ .vuepress/theme/components/JustifiedFlickrGallery.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        name: "justified-layout"
+    }
+</script>
+
+<style scoped>
+
+</style>
