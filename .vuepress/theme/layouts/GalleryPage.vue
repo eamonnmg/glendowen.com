ee900151c25189daa2219bd2d590fa6110e2beda
eamonnmcgonigle <mcgonigle.eamonn@gmail.com>
2019-04-22 23:00:58 +0100

    remove date author form page template

--- .vuepress/theme/layouts/GalleryPage.vue
+++ .vuepress/theme/layouts/GalleryPage.vue
@@ -4,7 +4,6 @@
             <article>
                 <header class="mb-6">
                     <h1>{{$page.frontmatter.title}}</h1>
-                    <span>April 3, 2019 By eamonn</span>
                 </header>
                 <Content></Content>
             </article>
