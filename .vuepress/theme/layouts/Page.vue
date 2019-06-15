3e6f5fdc2f686ddc6d11dc7b468d2cc9ebd43ae4
eamonnmcgonigle <mcgonigle.eamonn@gmail.com>
2019-06-15 17:14:32 +0100

    page mobile styles

--- .vuepress/theme/layouts/Page.vue
+++ .vuepress/theme/layouts/Page.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="mx-auto max-w-xl">
-        <main class="container border p-6 sm:m-6 ">
+    <div class="mx-auto max-w-xl flex justify-center">
+        <main class="container sm:border p-3 sm:p-6 sm:m-6 ">
             <article>
                 <header class="mb-6">
                     <h1 class="heading">{{$page.frontmatter.title}}</h1>
